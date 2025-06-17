20946012cd731ea3674ff2022793c07ea8ee2476
Paul Brandes <paulbrandes@gmx.de>
2025-06-17 15:29:54 +0100

    Create 07.1_run_GxE.sh

---
+++ 07.1_run_GxE.sh
@@ -0,0 +1,21 @@
+#$ -S /bin/bash
+#$ -l h_rt=06:0:0
+#$ -l tmem=20G
+#$ -l h_vmem=20G
+#$ -cwd
+#$ -j y
+#$ -N run_GWAS
+
+#/share/apps/plink-1.90-beta-6.10/plink --bfile DIVERGE_QCed --logistic --out gwas_results
+
+/share/apps/genomics/plink-2.0/bin/plink2 \
+  --pfile DIVERGE_QCed \
+  --pheno covariates.txt \
+  --pheno-name PHENO1 \
+  --covar covariates.txt \
+  --covar-name early_domestic_issues,SEX,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8 \
+  --parameters 1, 3-12, 13 \
+  --glm interaction\
+  --out gwas_results
+
+# 1, refers to Additive effect, 3-12 to the covariates, and 13 to the interaction between variants and early_domestic_issues
