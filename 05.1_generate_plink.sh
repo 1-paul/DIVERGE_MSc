2007abf7a265ada069109ee1ac9b61d9469a39da
Paul Brandes <paulbrandes@gmx.de>
2025-06-17 12:38:46 +0100

    Update 05.1_generate_plink.sh

--- 05.1_generate_plink.sh
+++ 05.1_generate_plink.sh
@@ -8,16 +8,22 @@
 #$ -j y
 #$ -N generate_plink_files
 
-inputfile="../munim_workspace/QC_pipeline_20250514/00_plink_files/05_sex_check_removed_xhcrom_abnormality"
-hardyweinberg_snps="../munim_workspace/QC_pipeline_20250514/08_hardy_weinberg_20250602/meta_snps.txt"
-removed_relatives="../munim_workspace/QC_pipeline_20250514/00_reports/06_relatedness_relatives_kingrobust.txt"
+inputfile="/cluster/project2/DIVERGE/munim_workspace/QC_pipeline_20250514/00_plink_files/05_sex_check_removed_xhcrom_abnormality"
+hardyweinberg_snps="/cluster/project2/DIVERGE/munim_workspace/QC_pipeline_20250514/08_hardy_weinberg_20250602/meta_snps.txt"
+removed_relatives="/cluster/project2/DIVERGE/munim_workspace/QC_pipeline_20250514/00_reports/06_relatedness_relatives_kingrobust.txt"
+
+awk 'NR > 1 && $5 == "PROBLEM" {print $2}' /cluster/project2/DIVERGE/munim_workspace/unfiltered/merged.sexcheck > /cluster/project2/DIVERGE/munim_workspace/ids_sexmismatch.txt
+sex_mismatch="/cluster/project2/DIVERGE/munim_workspace/ids_sexmismatch.txt"
 
 ##the current input file has gone through QC up to the end of the sex checks, the following command removes snps 
 ##that have failed a meta-analysis of hardy weinberg on different population clusters
 /share/apps/plink-1.90-beta-6.10/plink --bfile "$inputfile" --exclude "$hardyweinberg_snps" --make-bed --out ./DIVERGE_QCed_initial --allow-extra-chr
 
 ###removing close relatives here
-/share/apps/plink-1.90-beta-6.10/plink --bfile DIVERGE_QCed_initial --remove "$removed_relatives" --make-bed --out DIVERGE_QCed --allow-extra-chr
+/share/apps/plink-1.90-beta-6.10/plink --bfile DIVERGE_QCed_initial --remove "$removed_relatives" --make-bed --out DIVERGE_QCed1 --allow-extra-chr
+
+###removing close relatives here
+/share/apps/plink-1.90-beta-6.10/plink --bfile DIVERGE_QCed1 --remove "$sex_mismatch" --make-bed --out DIVERGE_QCed2 --allow-extra-chr
 
 
 ###### populate the phenotype column
@@ -29,4 +35,4 @@ rm DIVERGE_QCed.fam
 mv DIVERGE_QCed_.fam DIVERGE_QCed.fam
 
 ##convert to plink2
-/share/apps/genomics/plink-2.0/bin/plink2 --bfile DIVERGE_QCed --make-pgen --out DIVERGE_QCed
+/share/apps/genomics/plink-2.0/bin/plink2 --bfile DIVERGE_QCed2 --make-pgen --out DIVERGE_QCed
