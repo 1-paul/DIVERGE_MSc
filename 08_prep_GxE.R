89e7ba13934b5ad2d5b1c6c10304c91fd07bb674
Paul Brandes <paulbrandes@gmx.de>
2025-06-20 12:57:51 +0100

    Update 08_prep_GxE.R

--- 08_prep_GxE.R
+++ 08_prep_GxE.R
@@ -1,19 +1,35 @@
 library(dplyr)
 
+
 # Filter phenotype
 early_domestic_issues_df <- phenotype %>%
-	select(subject_id, early_domestic_issues)
+    select(subject_id, early_domestic_issues)
+
 
 # Load covariates file
 covariates <- read.table(
-	"/cluster/project2/DIVERGE/20250605_GWAS/covariates.txt",
-	header = TRUE,
-	sep = "\t",          
-	comment.char = ""    # Disable skipping lines starting with #
+    "/cluster/project2/DIVERGE/20250605_GWAS/covariates.txt",
+    header = TRUE,
+    sep = "\t",          
+    comment.char = ""    # Disable skipping lines starting with #
 )
 
+
 # Merge by ID, just adding early_domestic_issues
-combined_df <- merge(covariates, early_domestic_issues_df, by.x = "IID", , by.y = "subject_id")
+combined_df <- merge(covariates, early_domestic_issues_df, by.x = "IID", by.y = "subject_id")
+
+
+# Load freeze2 IDs file
+freeze2_ids <- read.table(
+    "/cluster/project2/DIVERGE/munim_workspace/QC_pipeline_20250514/09_batch_effects/freeze2_ids.txt",
+    header = FALSE,
+    col.names = c("FID", "IID")  # Naming columns for clarity
+)
+
+
+# Create freeze variable (2 for freeze 2 individuals, 1 for freeze 1 individuals)
+combined_df <- combined_df %>%
+  mutate(freeze = if_else(IID %in% freeze2_ids$IID, 2, 1))
 
 
 # Output new covariates file
