4a8b8644760241acc1d34a2818ea968b3f10c90f
Paul Brandes <paulbrandes@gmx.de>
2025-07-04 10:23:16 +0100

    Added post age-matching regression + improved readability

--- 03_identify_risk_factors.R
+++ 03_identify_risk_factors.R
@@ -1,3 +1,18 @@
+#############################################################################################
+### Summary:
+### 1. Define list of environmental risk factors
+### 2. Run regressions for each one:
+###   2.1. Without any covariates
+###   2.2. With age, sex, and ethnicity as covariates
+###   2.3. After age matching
+#############################################################################################
+
+
+library(survival)
+library(dplyr)
+
+
+# Define risk factors
 predictors <- c(
   "early_courts_issues",
   "early_forced_leave_home",
@@ -14,7 +29,8 @@ predictors <- c(
 
 
 
-### Log regression
+### Logistic regression without covariates -----------------------------------------------------------------------------------------------------------------------------------------
+# Establish storage variables
 pvals <- numeric(length(predictors))
 estimates <- numeric(length(predictors))
 names(pvals) <- names(estimates) <- predictors
@@ -45,20 +61,21 @@ print(results_df)
 
 
 
-### Log regression with age
+### Logistic regression with age as a covariate -----------------------------------------------------------------------------------------------------------------------------------------
+# Establish storage variables
 pvals <- numeric(length(predictors))
 estimates <- numeric(length(predictors))
 names(pvals) <- names(estimates) <- predictors
 
-# Run logistic regressions and extract p-values and estimates
+# Run logistic regressions with covariates and extract p-values and estimates
 for (i in seq_along(predictors)) {
-  formula <- as.formula(paste("subject_type_logical ~", predictors[i], "+ screener_age"))
+  formula <- as.formula(paste("subject_type_logical ~", predictors[i], "+ screener_age + screener_sex + ethnic_self_provincial"))
   model <- glm(formula, data = phenotype, family = binomial)
   smry <- summary(model)
   
-  # Extract p-value for the predictor
+  # Extract p-value
   pvals[i] <- coef(smry)[predictors[i], "Pr(>|z|)"]
-  # get the odds
+  # Extract odds
   estimates[i] <- exp(coef(smry)[predictors[i], "Estimate"])
 }
 
@@ -74,3 +91,37 @@ results_df <- data.frame(
 )
 
 print(results_df)
+
+
+
+### Conditional logistic regressions for age-matched case/controls after running "02_age_matching.R" ----------------------------------------------------------------------------------------------
+# Establish storage variables
+pvals <- numeric(length(predictors))
+estimates <- numeric(length(predictors))
+names(pvals) <- names(estimates) <- predictors
+
+# Run conditional logistic regressions and extract p-values and estimates
+for (i in seq_along(predictors)) {
+  formula <- as.formula(paste("subject_type_logical ~", predictors[i], "+ strata(pair_id)"))
+  model <- clogit(formula, data = matched_data_caliper)
+  smry <- summary(model)
+  
+  # Extract p-value
+  pvals[i] <- coef(smry)[predictors[i], "Pr(>|z|)"]
+  # Extract odds
+  estimates[i] <- coef(smry)[predictors[i], "exp(coef)"]
+}
+
+# Apply multiple testing correction
+pvals_corrected <- p.adjust(pvals, method = "bonferroni")
+
+# Combine into a data frame
+results_df <- data.frame(
+  Predictor = predictors,
+  Odds = estimates,
+  P_Value = pvals,
+  Adjusted_P = pvals_corrected
+)
+
+print(results_df)
+
